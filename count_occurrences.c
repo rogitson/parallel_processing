140a00d8fac3593511c3b90408a7618e1b0e9fbb
Rogitson <omarm.elaraby2000@gmail.com>
2022-11-17 17:46:39 +0200

    ✨feature: updated count_occurrences.c
    added counting occurrences by parallelizing the data

--- count_occurrences.c
+++ count_occurrences.c
@@ -6,7 +6,9 @@
 
 char *FileToString(const char *filename);
 int *parCountTask(char *buffer);
-void *countChar(void *p);
+void *countCharTask(void *p);
+int *parCountData(char *buffer);
+void *countCharData(void *p);
 
 struct s
 {
@@ -17,6 +19,9 @@ struct s
 
 // Maximum number of characters to count through.
 int maxlen = 10000;
+int threads = 16;
+long length;
+pthread_mutex_t lock;
 
 int main()
 {
@@ -32,10 +37,17 @@ int main()
     end = clock();
     time = (long double)(end - start) / CLOCKS_PER_SEC;
 
-    for (int i = 0; i < 128; ++i)
-        printf("ASCII #%d: %c -> %d\n", i, i, *(ans + i));
+    printf("Time taken to count occurrences while parallelizing the task is %Lf\n", time);
 
-    printf("Time taken to count occurrences parallelly is %Lf\n", time);
+    start = clock();
+    ans = parCountData(buffer);
+    end = clock();
+    time = (long double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("Time taken to count occurrences while parallelizing the data is %Lf\n", time);
+
+    // for (int i = 0; i < 128; ++i)
+    //     printf("ASCII #%d: %c -> %d\n", i, i, *(ans + i));
 
     // Cleanup.
     free(buffer);
@@ -46,7 +58,7 @@ int main()
 char *FileToString(const char *filename)
 {
     char *buffer = 0;
-    long length;
+    // long length;
     FILE *f = fopen(filename, "r");
 
     if (f)
@@ -79,7 +91,7 @@ int *parCountTask(char *buffer)
         (*t).arr = ans;
         (*t).buf = buffer;
         (*t).c = i;
-        pthread_create(tid + i, NULL, countChar, (void *)t);
+        pthread_create(tid + i, NULL, countCharTask, (void *)t);
     }
     for (int i = 0; i < 128; ++i)
         pthread_join(*(tid + i), NULL);
@@ -88,7 +100,7 @@ int *parCountTask(char *buffer)
     return ans;
 }
 
-void *countChar(void *p)
+void *countCharTask(void *p)
 {
     struct s *t = (struct s *)p;
     char *buffer = (*t).buf;
@@ -103,4 +115,79 @@ void *countChar(void *p)
                 break;
         }
     }
+}
+
+int *parCountData(char *buffer)
+{
+    int *ans = malloc(sizeof(int) * 128);
+    pthread_t *tid = malloc(sizeof(pthread_t) * threads);
+
+    // Set results array to 0.
+    for (int i = 0; i < 128; ++i)
+        *(ans + i) = 0;
+
+    if (length <= threads)
+    {
+        for (int i = 0; i < length; ++i)
+        {
+            struct s *t = malloc(sizeof(struct s));
+            (*t).arr = ans;
+            (*t).buf = buffer;
+            (*t).c = 1;
+            pthread_create(tid + i, NULL, countCharData, (void *)t);
+        }
+    }
+    else
+    {
+        // Split elements evenly across threads
+        // r number of threads will have 1 extra element
+        int r = length % threads;
+        int numElements = (length - r) / threads;
+
+        for (int i = 0, t = r; i < threads; ++i, --t)
+        {
+            // Store both input element addresses and result address in a struct to pass it to the thread.
+            struct s *t = malloc(sizeof(struct s));
+            (*t).arr = ans;
+            (*t).buf = (t > 0) ? buffer + (i * (numElements + 1)) : buffer + (i * numElements + r);
+            (*t).c = (t > 0) ? (numElements + 1) : numElements;
+            pthread_create(tid + i, NULL, countCharData, (void *)t);
+        }
+    }
+
+    for (int i = 0; i < threads; ++i)
+        pthread_join(*(tid + i), NULL);
+
+    free(tid);
+    return ans;
+}
+
+void *countCharData(void *p)
+{
+    struct s *t = (struct s *)p;
+    char *buffer = (*t).buf;
+    int *arr = malloc(sizeof(int) * 128);
+
+    // Set results array to 0.
+    for (int i = 0; i < 128; ++i)
+        *(arr + i) = 0;
+
+    if (buffer)
+    {
+        for (int i = 0; i < (*t).c; ++i)
+        {
+            *(arr + *(buffer + i)) += 1;
+            if (*(buffer + i) == '\0')
+                break;
+        }
+    }
+
+    pthread_mutex_lock(&lock);
+    for (int i = 0; i < 128; ++i)
+    {
+        (*t).arr[i] += arr[i];
+    }
+    pthread_mutex_unlock(&lock);
+
+    free(arr);
 }
